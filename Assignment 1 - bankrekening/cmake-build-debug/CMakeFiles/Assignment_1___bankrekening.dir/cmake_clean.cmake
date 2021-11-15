25da29eb034d046e0af41764a7403c6def090aae
Wridzer Kamphuis <wridzer12@gmail.com>
2021-11-15 13:06:46 +0100

    init first assignment

---
+++ Assignment 1 - bankrekening/cmake-build-debug/CMakeFiles/Assignment_1___bankrekening.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Assignment_1___bankrekening.exe"
+  "Assignment_1___bankrekening.exe.manifest"
+  "Assignment_1___bankrekening.pdb"
+  "CMakeFiles/Assignment_1___bankrekening.dir/main.cpp.obj"
+  "libAssignment_1___bankrekening.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Assignment_1___bankrekening.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
