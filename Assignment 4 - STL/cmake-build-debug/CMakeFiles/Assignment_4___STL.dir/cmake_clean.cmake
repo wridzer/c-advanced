1d0ec239fb1c5f63bc2d480fd7abd3faa5355a65
Wridzer Kamphuis <wridzer12@gmail.com>
2021-11-27 19:28:30 +0100

    working on assignment 4

---
+++ Assignment 4 - STL/cmake-build-debug/CMakeFiles/Assignment_4___STL.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Assignment_4___STL.exe"
+  "Assignment_4___STL.exe.manifest"
+  "Assignment_4___STL.pdb"
+  "CMakeFiles/Assignment_4___STL.dir/main.cpp.obj"
+  "libAssignment_4___STL.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Assignment_4___STL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Assignment 4 - STL/main.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <string>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+
+int main() {
+    std::vector<std::string> colours{"red", "green", "white", "blue", "orange", "green", "orange", "black", "purple"};
+    // gebruik functies uit <algorithm> en <functional> om (steeds vanuit een ORIGINELE copie van deze vector)
+    // 1) de vector in 2 nieuwe vectoren te splitsen: 1 met alles wat alfabetisch voor 'purple' komt, 1 met alles er na
+    /*for (int i = 0; i < colours.size(); i++) {
+        if (colours[i] == "purple"){
+            iterator = i;
+        }
+    }
+    std::vector<int> split_lo(colours.begin(), colours.begin() + iterator);
+    std::vector<int> split_hi(colours.begin() + iterator, colours.end());
+    for (auto a : split_lo) {
+        std::cout << a << std::endl;
+    }
+    for (auto a : split_hi) {
+        std::cout << a << std::endl;
+    }*/
+
+    {
+        std::vector<std::string> colours{"red", "green", "white", "blue", "orange", "green", "orange", "black", "purple"};
+        // 2) alle elementen UPPERCASE te maken.
+        for (auto a : colours){
+            std::transform(a.begin(), a.end(),a.begin(), ::toupper);
+            std::cout << a << std::endl;
+        }
+    }
+    {
+        std::vector<std::string> colours{"red", "green", "white", "blue", "orange", "green", "orange", "black", "purple"};
+        // 3) alle dubbele te verwijderen
+        std::sort(colours.begin(), colours.end());
+        colours.erase(unique(colours.begin(), colours.end()), colours.end());
+        for (auto a : colours){
+            std::cout << a << std::endl;
+        }
+
+    }
+
+    // gebruik functies uit <algorithm> en <functional> om (steeds vanuit een ORIGINELE copie van deze vector)
+    {
+        std::vector<double> numbers{10, 324422, 6, -23, 234.5, 654.1, 3.1242, -9.23, 635};
+        // 1) alle negatieve elementen te verwijderen
+        auto it = remove_if(numbers.begin(), numbers.end(),  [](const int i) {return i < 0; });
+        numbers.erase(it, numbers.end());
+        for (auto a : numbers){
+            std::cout << a << std::endl;
+        }
+    }
+
+    {
+        std::vector<double> numbers{10, 324422, 6, -23, 234.5, 654.1, 3.1242, -9.23, 635};
+        // 2) voor alle elementen te bepalen of ze even of oneven zijn
+    }
+
+    {
+        std::vector<double> numbers{10, 324422, 6, -23, 234.5, 654.1, 3.1242, -9.23, 635};
+        // 3) de som, het gemiddelde, en het product van alle getallen te berekenen
+        int gem = std::accumulate(numbers.begin(), numbers.end(), 0) / numbers.size();
+        std::cout << "gemiddelde is: " << gem << std::endl;
+    }
+
+    return 0;
+}
